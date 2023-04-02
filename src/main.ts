f0e4173aef5c0b5496cdc330a6602cc7be8afb67
Enrique Ansa <enriqueansa@gmail.com>
2023-04-02 00:35:15 -0400

    enabling corss

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { config } from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 config()
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'https://todolist-eanc.netlify.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.enableCors(corsOptions);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter);
   await app.listen(process.env.PORT || 3000);
